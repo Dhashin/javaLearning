ab55c76756294c33e582777c10cd6cd7b52eb3bc
dhashin.moodley <dhashin.moodley@fisglobal.com>
2019-06-13 16:31:45 +0200

    First chapter stuff

---
+++ OCA/buildingBlocks/Comments.java
@@ -0,0 +1,22 @@
+public class Comments{
+
+	// This is a single line comment
+
+	/* Multi 
+	*line 
+	*Comment
+	*/
+
+	/*
+	Also a 
+	multi-line
+	comment
+	*/
+
+	/**
+	*Javadoc multiple-line comment
+	*@author Dhashin Moodley
+	*/
+
+
+}
