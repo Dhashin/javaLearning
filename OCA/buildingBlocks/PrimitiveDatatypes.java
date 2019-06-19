211eb200eade55af5c441d1313ed60c9d1c6bd9d
dhashin.moodley <dhashin.moodley@fisglobal.com>
2019-06-19 16:33:43 +0200

    .

---
+++ OCA/buildingBlocks/PrimitiveDatatypes.java
@@ -0,0 +1,30 @@
+public class PrimitiveDatatypes{
+	
+boolean condition; 	// True or False
+
+byte byteValue; 	// 8-bit integral value. Can hold a value from -128 to 127
+
+short shortValue; 	// 16-bit integral value
+ 
+int intValue; 		// 32-bit integral value
+
+long longValue; 	// 64-bit integral value
+
+float floatValue; 	// 32-bit floating-point value
+
+double doubleValue; // 64-bit floating-point value
+
+char character; 	// 16-bit unicode value
+
+public static void main(String []args){
+System.out.println("The max int value is " +Integer.MAX_VALUE);
+System.out.println("The max byte value is " +Byte.MAX_VALUE);
+}
+
+
+
+
+
+
+
+}
